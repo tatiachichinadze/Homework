48b58b4ec5a52cb7a367e86f3a7ceca6a2affe35
Tatia Chichinadze <echichinadze@credo.ge>
2026-03-15 00:28:47 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+//დაწერეთ კონსოლური აპლიკაცია რომელიც შავი ეკრანიდან მიიღებს ორ input-ს და ეკრანზსე დაბეჭდავს იმას რომელიც უფრო მეტია
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        int a = int.Parse(Console.ReadLine());
+//        int b = int.Parse(Console.ReadLine());
+
+//        if (a > b)
+//            Console.WriteLine(a);
+//        else
+//            Console.WriteLine(b);
+//    }
+//}
+
+//დაწერეთ კონსულირი აპლიკაცია რომელიც შავი ეკრანიდან მიიღებს რაიმე input-ს და დაბეჭდავს მასზე მცირე ყველა კენტ რიცხვს. მაგ: 12-> 1, 3, 5, 7, 9, 11
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        int n = int.Parse(Console.ReadLine());
+
+//        for (int i = 1; i < n; i += 2)
+//        {
+//            Console.Write(i + " ");
+//        }
+//    }
+//}
+
+//დაწერეთ კონსულური აპლიკაცია რომელიც გადაწოდებულ რიცხვზე გვეტყვუს მარტივია თუ შდგენილი.
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine("Composite");
+                return;
+            }
+        }
+
+        Console.WriteLine("Prime");
+    }
+}
+
